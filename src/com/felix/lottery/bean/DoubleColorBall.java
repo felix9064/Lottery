1625b7ac915be882c043240ee3973751a688dccd
felix9064 <zhang1zhi7hua5@gmail.com>
2017-06-23 09:27:35 +0800

    忽略idea目录和项目文件，新增项目相关代码

---
+++ src/com/felix/lottery/bean/DoubleColorBall.java
@@ -0,0 +1,11 @@
+package com.felix.lottery.bean;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * Author: Felix
+ * Date: 2017/6/22
+ * Time: 20:24
+ */
+public class DoubleColorBall {
+}
---
+++ src/com/felix/lottery/test/LotteryTest.java
@@ -0,0 +1,11 @@
+package com.felix.lottery.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * Author: Felix
+ * Date: 2017/6/22
+ * Time: 21:15
+ */
+public class LotteryTest {
+}
