820f33469de5877a6ea875856f69e7bbf2bb2522
felix9064 <zhang1zhi7hua5@gmail.com>
2017-06-23 09:30:28 +0800

    修改项目代码和忽略文件

--- src/com/felix/lottery/bean/DoubleColorBall.java
+++ src/com/felix/lottery/bean/DoubleColorBall.java
@@ -8,4 +8,97 @@ package com.felix.lottery.bean;
  * Time: 20:24
  */
 public class DoubleColorBall {
+
+    private String kaijqs;
+    private String red1;
+    private String red2;
+    private String red3;
+    private String red4;
+    private String red5;
+    private String red6;
+    private String blue;
+
+    public void setBlue(String blue) {
+        this.blue = blue;
+    }
+
+    public String getBlue() {
+
+        return blue;
+    }
+
+    private String kjdate;
+
+    public String getKaijqs() {
+        return kaijqs;
+    }
+
+    public String getRed1() {
+        return red1;
+    }
+
+    public String getRed2() {
+        return red2;
+    }
+
+    public String getRed3() {
+        return red3;
+    }
+
+    public String getRed4() {
+        return red4;
+    }
+
+    public String getRed5() {
+        return red5;
+    }
+
+    public String getRed6() {
+        return red6;
+    }
+
+    public String getKjdate() {
+        return kjdate;
+    }
+
+    public void setKaijqs(String kaijqs) {
+        this.kaijqs = kaijqs;
+    }
+
+    public void setRed1(String red1) {
+        this.red1 = red1;
+    }
+
+    public void setRed2(String red2) {
+        this.red2 = red2;
+    }
+
+    public void setRed3(String red3) {
+        this.red3 = red3;
+    }
+
+    public void setRed4(String red4) {
+        this.red4 = red4;
+    }
+
+    public void setRed5(String red5) {
+        this.red5 = red5;
+    }
+
+    public void setRed6(String red6) {
+        this.red6 = red6;
+    }
+
+    public void setKjdate(String kjdate) {
+        this.kjdate = kjdate;
+    }
+
+    @Override
+    public String toString() {
+        return "开奖期数：" + this.getKaijqs() +
+                "\r\n开奖日期：" + this.getKjdate() +
+                "\r\n中奖红色球：" + this.getRed1() + "," + this.getRed2() + "," +
+                this.getRed3() + "," + this.getRed4() + "," + this.getRed5() + "," +
+                this.getRed6() + "\r\n中奖蓝色球：" + this.getBlue();
+    }
 }
--- src/com/felix/lottery/test/LotteryTest.java
+++ src/com/felix/lottery/test/LotteryTest.java
@@ -1,5 +1,14 @@
 package com.felix.lottery.test;
 
+import com.felix.lottery.bean.DoubleColorBall;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+
 /**
  * Created with IntelliJ IDEA.
  * Description:
@@ -8,4 +17,20 @@ package com.felix.lottery.test;
  * Time: 21:15
  */
 public class LotteryTest {
+    public static void main(String[] args) throws IOException {
+        String resource = "mybatis-config.xml";
+        Reader reader = Resources.getResourceAsReader(resource);
+
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(reader);
+        SqlSession session = sqlSessionFactory.openSession();
+        /**
+         * 映射sql的标识字符串com.felix.lottery.mapping.LotteryMapper.getKaijhm
+         * getKaijhm是select标签的id属性值，通过select标签的id属性值就可以找到要执行的SQL
+         */
+        String statement = "com.felix.lottery.mapping.LotteryMapper.getKaijhm";//映射sql的标识字符串
+        //执行查询返回一个唯一user对象的sql
+        DoubleColorBall ssq = session.selectOne(statement, "2017070");
+        System.out.println(ssq.toString());
+    }
 }
